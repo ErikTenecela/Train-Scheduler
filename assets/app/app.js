0d8af6cf782808d436ae90ed215f9ab663458013
Erik Tenecela <eriktenecela13@gmail.com>
2019-12-20 22:04:41 -0500

    getting-user-input

--- assets/app/app.js
+++ assets/app/app.js
@@ -4,3 +4,24 @@ const config = {
   databaseURL: "https://train-times-93583.firebaseio.com",
   storageBucket: "train-times-93583.appspot.com"
 };
+
+firebase.initializeApp(config);
+
+const trainData = firebase.database();
+
+$("#add-train-btn").on("click", function(event) {
+  event.preventDefault();
+
+  const trainName = $("#train-name-input")
+    .val()
+    .trim();
+  const destination = $("#destination-input")
+    .val()
+    .trim();
+  const firstTrain = $("#first-train-input")
+    .val()
+    .trim();
+  const frequency = $("#frequency-input")
+    .val()
+    .trim();
+});
