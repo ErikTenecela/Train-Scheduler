b92d34bf28139a4667564d34be62159f6fa91c61
Erik Tenecela <eriktenecela13@gmail.com>
2019-12-20 21:48:47 -0500

    fixed-html-added-config

---
+++ assets/app/app.js
@@ -0,0 +1,6 @@
+const config = {
+  apiKey: "AIzaSyCcPFcbAjIsgXGQwE-A3AcOXkeD40qypE8",
+  authDomain: "train-times-93583.firebaseapp.com",
+  databaseURL: "https://train-times-93583.firebaseio.com",
+  storageBucket: "train-times-93583.appspot.com"
+};
